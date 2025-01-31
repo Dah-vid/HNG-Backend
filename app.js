5356268a38c7da04d075cb57eaff5836b86453f0
David Okunfolami <34836799+Dah-vid@users.noreply.github.com>
2025-01-31 21:11:14 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+// CORS Handling
+const cors = require("cors");
+app.use(
+    cors({
+        origin: "*",
+    })
+)
+const PORT = process.env.PORT || 3000;
+
+app.get("/status", (request, response) => {
+    const status = {
+        email_address: "davidtopair@gmail.com",
+        current_datetime: new Date().toISOString(),
+        github: "https://github.com/Dah-vid/HNG-Backend"
+    };
+    
+    response.send(status);
+});
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+});
